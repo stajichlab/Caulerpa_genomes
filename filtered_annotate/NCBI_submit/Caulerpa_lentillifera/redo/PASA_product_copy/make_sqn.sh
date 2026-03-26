cd773c91653acd8f1c8738cba818948cd7aacb7e
Jason Stajich <jasonstajich.phd@gmail.com>
2026-03-26 08:58:01 -0700

    Clent updated NCBI files

---
+++ filtered_annotate/NCBI_submit/Caulerpa_lentillifera/redo/PASA_product_copy/make_sqn.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -c  2 --mem 2gb -N 1 -n 1 
+
+module load ncbi-table2asn
+table2asn -l paired-ends -l align-genus -V v -M n -c efx -i Caulerpa_lentillifera.fsa -usemt two \
+	-o Caulerpa_lentillifera.sqn -Z -t Clent.sbt -euk  -j "[organism=Caulerpa lentillifera] [gcode=1]" \
+	-gap-type contig
+
+module load ncbi-asn_tools
+
+asn2all -i Caulerpa_lentillifera.sqn -f d -v Caulerpa_lentillifera.aa
+pigz -kf Caulerpa_lentillifera.sqn
